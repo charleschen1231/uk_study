e5b645e28da5316d6b9e0501e36c8295e685036e
Charles <charles69430@163.com>
2022-12-30 22:03:20 +0000

    second commit

--- src/main/java/leetcode_demo/AA.java
+++ src/main/java/leetcode_demo/AA.java
@@ -4,5 +4,6 @@ public class AA {
     public static void main(String[] args) {
         System.out.println("hhhhhhhhhh");
         System.out.println("0000000");
+        System.out.println("hahahaha");
     }
 }
